985b8d75e83e6244ea21fe9acae671767a336201
banaswaqqad <Banawaqqad7@gmail.com>
2021-03-03 23:13:55 +0200

    made some changes in app.js file

--- js/app.js
+++ js/app.js
@@ -1,97 +1,136 @@
 'use strict';
 let score =0;
 
-let userName=prompt('Hello friend,please enter your name to know you more ');
+let userName=prompt('Hello friend ,please enter your name to know you more ');
+
+while(!userName)
+{
+    userName=prompt('you are not allow to leave this empty');
+}
+
 alert('hello' + userName + '  you are welcome to our website')
 
 
-let she_he = prompt("do you think i am male ?");
-console.log(she_he);
-NO(she_he);
 
-
-let age = prompt("Do you think my age is over 30?");
-console.log(age);
-NO(age);
-let country = prompt("Do you think that I live in an Arab country?");
-console.log(country);
-YES(country);
-let major = prompt("Do you think that I studied a major related to information technology?");
-console.log(major)
-YES(major);
-let opinion = prompt("do you think i enjoy building websites");
-console.log(opinion)
-YES(opinion);
-
-
-function NO(answer)
+let she_he = prompt('do you think i am male ?');
+if ( she_he.toLowerCase() ==='no' || she_he.toLowerCase() ==='n' )
 {
-      
-    
-    if (answer=='no' || answer =='n'|| answer == 'N' || answer=='NO' )
-    {
-        alert("Bravooo, your answer is true ");
-        score=score+1;
-    }
-    else 
-    {
-        alert("SORRY YOUR ANSWER WAS WRONG, YOU CAN TRY AGAIN");
-    }
+    alert('Bravoo your answer is true');
+
+console.log('your answer was' + ' ' + she_he);
+score++;
 }
 
-
-function YES(answer1)
+else
 {
-
-    if (answer1=='yes' || answer1 =='YES' || answer1 =='y' || answer1 =='Y')
-    {
-        alert("Bravooo, your answer is true ");
-        score=score+1;
-    }
-    else 
-    {
-        alert("SORRY YOUR ANSWER WAS WRONG, YOU CAN TRY AGAIN");
-    }
+    alert('SORRY YOUR ANSWER WAS WRONG');
 }
-
-alert ('nice to meet you ' + '  ' + userName );
-
-
-
-
-let theNumber=45;
-
-for (let i = 0;i<4;i++)
-{
-    
- let guessnumber=parseInt( prompt(  userName + '  '+ 'how about trying to guess number' ));
-
-if(guessnumber>45)
-    {
-        alert('no,try again \n hint : the number is lower ');
-    }
-    else 
-    if (guessnumber< 45 )
-    {
-        alert('no,try again \n hint : the number is higher ');
-    }
-    else 
-    if(guessnumber == 45 )
-    {
-        alert('Bravoo , you are awesome')
-        score=score+1;
-        i=5;
-    }
-
-    if(i>=4)
-{
-
    
-        alert('sorry , the number was ' + ' ' + theNumber );
+
+
+let age = prompt('do you think i am over 30 ?');
+if(age.toLowerCase() ==='no' || age.toLowerCase()==='n')
+{
+    alert('Bravoo your answer is true ');
+
+console.log('your answer was' + ' ' + age);
+score++;
+}
+else
+{
+    alert('SORRY YOUR ANSWER WAS WRONG');
+}
+
+let country = prompt('Do you think that I live in an Arab country?');
+if(country.toLowerCase() ==='yes' || country.toLowerCase()==='y')
+{
+    alert('Bravoo your answer is true');
+
+console.log('your answer was' + ' ' + country);
+score++;
+}
+else
+{
+    alert('SORRY YOUR ANSWER WAS WRONG');
+}
+
+
+let major = prompt('Do you think that I studied a major relate to information technology?');
+if(major.toLowerCase() ==='yes' || major.toLowerCase() ==='y')
+{
+    alert('Bravoo your answer is true ');
+
+console.log('your answer was' + ' ' + major);
+score++;
+}
+
+else
+{
+    alert('SORRY YOUR ANSWER WAS WRONG');
+}
+
+let opinion = prompt('do you think i enjoy building websites');
+if(opinion.toLowerCase()==='yes' || opinion.toLowerCase()==='y')
+{
+    alert('Bravoo your answer is true ');
+
+console.log('your answer was' + ' ' + opinion);
+score++;
+}
+else
+{
+    alert('SORRY YOUR ANSWER WAS WRONG');
+}
+
+
+
+
+let attempts=3;
+let theNumber=45;
+let guessNumber = prompt('try to guess number');
+let x =0;
+while(x<attempts)
+{
+    if(x>attempts)
+    {
+        alert ('bye')
+    }
+
+if (guessNumber >theNumber )
+{
+alert('to high');
+guessNumber = prompt('try to guess number');
+
+x++
+if(x>attempts)
+{
+    alert ('bye')
+}
 
 }
-  
+else 
+if (guessNumber<theNumber)
+{ 
+    alert('too small')
+    guessNumber = prompt('try to guess number');
+
+x++;
+if(x>attempts)
+{
+    alert ('bye')
 }
+
+}
+else 
+
+if(guessNumber===theNumber)
+{
+    alert('bravo');
+    i>4;
+}
+
+}
+
 
 
 
