27ba25d3d226f7f5d377d82ba1e71e6c0201464b
banaswaqqad <Banawaqqad7@gmail.com>
2021-03-03 08:37:59 +0200

    made a new guessing quistions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let score =0;
 
 let userName=prompt('Hello friend,please enter your name to know you more ');
 alert('hello' + userName + '  you are welcome to our website')
@@ -8,6 +8,8 @@ alert('hello' + userName + '  you are welcome to our website')
 let she_he = prompt("do you think i am male ?");
 console.log(she_he);
 NO(she_he);
+
+
 let age = prompt("Do you think my age is over 30?");
 console.log(age);
 NO(age);
@@ -24,11 +26,12 @@ YES(opinion);
 
 function NO(answer)
 {
-
+      
     
-    if (answer=='no' || answer =='NO' || answer =='n' || answer =='N')
+    if (answer=='no' || answer =='n'|| answer == 'N' || answer=='NO' )
     {
         alert("Bravooo, your answer is true ");
+        score=score+1;
     }
     else 
     {
@@ -43,6 +46,7 @@ function YES(answer1)
     if (answer1=='yes' || answer1 =='YES' || answer1 =='y' || answer1 =='Y')
     {
         alert("Bravooo, your answer is true ");
+        score=score+1;
     }
     else 
     {
@@ -50,6 +54,76 @@ function YES(answer1)
     }
 }
 
-alert ('nice to meet you ' + userName );
+alert ('nice to meet you ' + '  ' + userName );
 
 
+
+
+let theNumber=45;
+
+for (let i = 0;i<4;i++)
+{
+    
+ let guessnumber=parseInt( prompt(  userName + '  '+ 'how about trying to guess number' ));
+
+if(guessnumber>45)
+    {
+        alert('no,try again \n hint : the number is lower ');
+    }
+    else 
+    if (guessnumber< 45 )
+    {
+        alert('no,try again \n hint : the number is higher ');
+    }
+    else 
+    if(guessnumber == 45 )
+    {
+        alert('Bravoo , you are awesome')
+        score=score+1;
+        i=5;
+    }
+
+    if(i>=4)
+{
+
+   
+        alert('sorry , the number was ' + ' ' + theNumber );
+
+}
+  
+}
+
+
+
+
+
+
+
+let cars =['audi','jaguar','bmw'];
+let i=0;
+while (i<6)
+{
+ let favcars = prompt( 'how about trying to guess my favourite car' );
+  if(favcars === cars[0] || favcars ===cars[1] || favcars === cars[2] )
+{
+    alert ('good job,that answer is correct ');
+    score=score+1
+  break;
+}
+else
+{
+alert('no,that is wrong');
+i++
+}
+if(i==6)
+{
+    alert ('sorry, you are out of the attempts,these are my favourite cars' ) 
+for(let i =0;i<cars.length;i++)
+{
+alert(cars[i] );
+}
+
+}
+ 
+}
+alert('your score is ' + score  )
